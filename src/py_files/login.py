be67105f163a143ac211849849f8f9382891bc2f
ZingerMarat <zingermarat@gmail.com>
2022-05-04 15:43:26 +0300

    Register validation

--- src/py_files/login.py
+++ src/py_files/login.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 csv_path = os.getcwd() + "\\src\\web\\files\\storage\\users.csv"
 
 def check_credentials(username, password):
@@ -26,20 +27,79 @@ def add_cred(username, password):
 def save_register(username, pass_1, pass_2):
     try:
         if username != "" and pass_1 != "" and pass_2 != "" and pass_1 == pass_2:
-            add_cred(username, pass_1)
-            msg = "success"
-            return msg
+            if check_username(username) == "success" and check_password(pass_1) == "success":
+                add_cred(username, pass_1)
+                msg = "success"
         else:
             msg = "failure"
-            return msg
+        return msg
     except Exception as Error:
         print(Error)
         msg = "failure"
         return msg
 
-def check_register(username, pass_1, pass_2):
-    pass
+def check_username(username):
+    #check if len of username between 6 and 8, contains only letters and max of 2 numbers
+    msg = "failure"
+    if 6<= len(username) <= 8:
+        count_dig = 0
+        let = re.compile(r'[a-zA-Z]')
+        dig = re.compile(r'[0-9]')
+        for i in username:
+            if dig.match(i):
+                count_dig += 1
+            elif not let.match(i):
+                return "failure"
 
+        if 0 <= count_dig <= 2:
+            msg = "success"
+        else:
+            msg = "failure"
+    return msg
+
+def check_password(password):
+    #check if len of password between 8 and 10, contains at least one number, one letter and one special character
+    msg = "failure"
+    if 8<= len(password) <= 10:
+        count_dig = 0
+        count_spec = 0
+        count_let = 0
+        dig = re.compile(r'[0-9]')
+        let = re.compile(r'[a-zA-Z]')
+        spec = re.compile(r'[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?]')
+        for i in password:
+            if dig.match(i):
+                count_dig += 1
+            elif let.match(i):
+                count_let += 1
+            elif spec.match(i):
+                count_spec += 1
+
+        if count_dig >= 1 and count_let >= 1 and count_spec >= 1:
+            msg = "success"
+        else:
+            msg = "failure"
+    return msg
+
+def check_ID(ID):
+    #check if ID is a valid
+    if len(ID) != 9:
+        return "failure"
+
+    IdList = list()
+    id_list = list(map(int, ID))
+    counter = 0
+
+    for i in range(9):
+        id_list[i] *= (i % 2) + 1
+        if id_list[i] > 9:
+            id_list[i] -= 9
+        counter += id_list[i]
+
+    if counter % 10 == 0:
+        return "success"
+    else:
+        return "failure"
 
 def login_user(user_name, password):
     try:
